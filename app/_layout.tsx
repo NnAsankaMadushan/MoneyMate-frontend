faefe74bdc9c78d9f1ab4786f3d99b9dcea1f433
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-06-02 20:39:42 +0530

    finalized dashboard page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,10 +8,10 @@ const RootLayout = () => {
   return (
   <View className='relative w-full flex-1'>
     <Slot />
-    <View style={styles.navigationBar}>
+    <View  style={styles.navigationBar}>
         <View style={styles.navi}>
           
-            <TouchableOpacity style={styles.menuItem}>
+            <TouchableOpacity style={styles.navItem}>
             <Link href={'/'}>
                 <Icon name="home" size={30} color="#5cb075" style={styles.icons} />
             </Link>
@@ -19,7 +19,7 @@ const RootLayout = () => {
               </TouchableOpacity>
           
          
-            <TouchableOpacity style={styles.menuItem}>
+            <TouchableOpacity style={styles.navItem}>
             <Link href={'/register'}>
                 <Icon name="user-plus" size={30} color="#5cb075" style={styles.icons} />
             </Link>
@@ -27,7 +27,7 @@ const RootLayout = () => {
               </TouchableOpacity>
           
           
-            <TouchableOpacity style={styles.menuItem}>
+            <TouchableOpacity style={styles.navItem}>
             <Link href={'/clients'}>
                 <Icon name="users" size={30} color="#5cb075" style={styles.icons} />
             </Link>
@@ -35,7 +35,7 @@ const RootLayout = () => {
               </TouchableOpacity>
          
           
-            <TouchableOpacity style={styles.menuItem}>
+            <TouchableOpacity style={styles.navItem}>
             <Link href={'/reports'}>
                 <Icon name="file" size={30} color="#5cb075" style={styles.icons} />
             </Link>
@@ -43,7 +43,7 @@ const RootLayout = () => {
               </TouchableOpacity>
       
           
-            <TouchableOpacity style={styles.menuItem}>
+            <TouchableOpacity style={styles.navItem}>
             <Link href={'/dashboard'}>
                 <Icon name="user" size={30} color="#5cb075" style={styles.icons} />
             </Link>
@@ -62,10 +62,13 @@ const RootLayout = () => {
 const styles = StyleSheet.create({
   navigationBar:{
     width:'100%',
-    backgroundColor:"#5cb075",
+    backgroundColor:"#f6f6f6",
     zIndex:50,
     height:90,
-    padding:10
+    padding:10,
+    borderTopColor: '#c9c9c9', 
+    // borderColor:'#fff',
+    borderWidth: 1, 
   },
   navi:{
     justifyContent: 'space-around',
@@ -74,14 +77,14 @@ const styles = StyleSheet.create({
     padding:5
   },
   naviText:{
-    color: '#fff',
+    color: '#5cb075',
     fontSize:18,
   },
   icons:{
     marginRight: 15,
-    color:'#fff',
+    color:'#5cb075',
   },
-  menuItem: {
+  navItem: {
     flexDirection: 'column', 
     alignItems: 'center',
   },
