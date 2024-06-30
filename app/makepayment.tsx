47973a176babf154335a6de0cd8b955322448ac6
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2024-06-30 13:51:08 +0530

    makepapyment screen

--- app/makepayment.tsx
+++ app/makepayment.tsx
@@ -1,11 +1,31 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text,TextInput} from 'react-native'
+import React, { useState } from 'react'
 import styles from './style';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Link } from 'expo-router';
 
 const Makepayment = () => {
+    const [nicNo,setNicNo] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Makepayment</Text>
+      <View style={styles.HeadStyle}>
+        <Link href={'/dashboard'}>
+          <Icon name="close" size={25} color="#d0d0d0" />
+        </Link>
+        <Text style={styles.HeadStyleText}>Make Payment</Text>
+        <Link href={'/register2'} style={styles.nextText}>
+              <Text>Next</Text>
+            </Link>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="User ID/NIC"
+          value={nicNo}
+          onChangeText={setNicNo}
+          editable={true}
+        />
+      </View>
     </View>
   )
 }
--- app/style.tsx
+++ app/style.tsx
@@ -6,6 +6,33 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: '#fff',
   },
+  HeadStyle: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginBottom: 25,
+  },
+  HeadStyleText: {
+    color: 'black',
+    fontSize: 34,
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    flex: 1,
+  },
+  input: {
+    backgroundColor: '#f0f0f0',
+    padding: 16,
+    borderRadius: 8,
+    marginVertical: 8,
+    fontSize: 16,
+    color: '#a0a0a0',
+  },
+  nextText:{
+    fontSize:18,
+    color:'#5cb075',
+    fontWeight:500
+  },
 });
 
 export default styles;
